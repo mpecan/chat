069ffdec7d28afc15de491a1d576766ed2823c72
Taj Pelc <tajpelc@gmail.com>
2017-03-29 22:13:57 +0200

    further fixes

--- src/react/js/views/App/index.jsx
+++ src/react/js/views/App/index.jsx
@@ -4,14 +4,14 @@ import React, { Component, PropTypes } from 'react';
 export default class App extends Component {
   static propTypes = {
     children: PropTypes.object,
-  }
+  };
 
 
   render() {
     const { children } = this.props;
 
     return (
-      <div className='App'>
+      <div className='App group'>
 
         <div className='Page'>
           { children }
